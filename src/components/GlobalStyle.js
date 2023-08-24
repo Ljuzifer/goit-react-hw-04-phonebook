ae09d1534746f5579c685dd216d7b00fa7adad8a
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-08-24 17:42:27 +0300

    done

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -4,7 +4,7 @@ import 'modern-normalize';
 export const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
-  background-color: blanchedalmond;
+  background-color: whitesmoke;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
